a01b7142d6b550f8c64300c1b26e6b71ea719786
Kubo Travis <kubo.travis@protonmail.com>
2018-01-27 16:26:29 +0530

    Added sample python scripts

---
+++ Python-Notes/Samples/01_arg_validation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+
+# Usage function
+def usage():
+    print('Please supply atleast one ARG to this script')
+    sys.exit(1)
+
+# Get the total no.of arg passed
+arglen=len(sys.argv)-1
+
+if arglen == 0:
+    usage()
+else:
+    print("you have supplied totally"), arglen, ("arguments")
+    print("Let's loop thought the each arguments")
+    count = 1
+    while count <= arglen:
+        print("The"), count,("ARG is"), sys.argv[count]
+        count = count+1
+
+print("---END---")
+        
